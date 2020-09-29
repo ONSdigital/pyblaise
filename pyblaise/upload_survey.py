dcade4fe56c5b57ad5fe171465d7ed02eb4ed130
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-09-29 11:29:13 +0100

    Upload a survey.

--- pyblaise/upload_survey.py
+++ pyblaise/upload_survey.py
@@ -1,19 +1,19 @@
 import requests
 from .soap_utils import build_uri
 
-REQUEST_PATH = "/Blaise/Administer/Services/REST/ftf/OPN2004A/8a2d121e-6dba-4bf5-a6bf-d1b0e6c36160/uploadpackage"
 
-
-def upload_survey(protocol, host, port, token, filename):
+def upload_survey(protocol, host, port, server_park, survey_name, survey_id, token, filename):
     headers = {
         "Authorization": "Bearer %s" % token,
         "Content-Type": "application/binary",
         "Expect": "100-continue",
     }
 
+    REQUEST_PATH = f"/Blaise/Administer/Services/REST/{server_park}/{survey_name}/{survey_id}/uploadpackage"
+
     with open(filename, "rb") as payload:
         R = requests.post(
-            build_uri(protocol, host, port, REQUEST_PATH), headers=headers, data=payload
+            build_uri(protocol, host, port, REQUEST_PATH), headers=headers, data=payload,
         )
 
     return R.status_code
