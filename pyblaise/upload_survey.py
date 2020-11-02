84ceb2c803769fe577022ac8a8f6177af623ad40
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-11-02 17:26:13 +0000

    Read only for Survey Upload

--- pyblaise/upload_survey.py
+++ pyblaise/upload_survey.py
@@ -13,7 +13,7 @@ def upload_survey(
 
     REQUEST_PATH = f"/Blaise/Administer/Services/REST/{server_park}/{survey_name}/{survey_id}/uploadpackage"
 
-    with open(filename, "rb") as payload:
+    with open(filename, "r") as payload:
         R = requests.post(
             build_uri(protocol, host, port, REQUEST_PATH),
             headers=headers,
